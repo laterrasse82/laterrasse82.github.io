ce7b6260302061580576872e1d119e19b57c4f3d
mathiasfou <mathias.fou@gmail.com>
2022-04-25 12:26:22 +0200

    upload carte.py

--- carte.py
+++ carte.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-bieres_pression = [{"nom":"Ratz blanche",
-            "description":"bla bla bla",
-            "prix":"4.5€",
+bieres_pression = [{"nom":"19 30 blonde",
+            "description":"La 19 30 Blonde est une bière sèche et fruitée, de type Pale Ale, qui vous rafraîchira par sa douce amertume et ses notes citronnées.",
+            "prix":"3.5€",
             "qtt":"25cl",
             "image":"assets/img/no_img.jpg"
                 },
-            {"nom":"Ratz blonde",
-            "description":"bla bla bla",
-            "prix":"4.5€",
-            "qtt":"25cl",
-            "image":"assets/img/no_img.jpg"
-                },
-            {"nom":"Ratz IPA",
-            "description":"bla bla bla",
-            "prix":"4.5€",
+            {"nom":"19 30 IPA",
+            "description":"Cette 19 30 IPA  India Pale Ale est une bière maltée dont la franche amertume se mêle à des notes de fruits exotiques apportées par les houblons.",
+            "prix":"4€",
             "qtt":"25cl",
             "image":"assets/img/no_img.jpg"
                 }
@@ -39,41 +33,35 @@ bieres_bouteille = [{"nom":"Ratz blanche",
             "qtt":"33cl",
             "image":"assets/img/no_img.jpg"
                 },
-            {"nom":"Ocale blanche",
-            "description":"bla bla bla",
+            {"nom":"OC’ALE Blanche de Printemps",
+            "description":"Malt d’orge et de blé. Assemblage de houblons aromatiques et amers. Attaque en bouche fraîche, douce et fleurie. Persistance de saveurs douces de fleurs. Fruit d’un partenariat avec les Vergers de Touticaut, à Dunes (82).",
             "prix":"4.5€",
             "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/bieres/ocale_blanche.jpg"
                 },
-            {"nom":"Ocale blonde",
-            "description":"bla bla bla",
+            {"nom":"OC’ALE Blonde",
+            "description":"Malt d’orge Pilsen. Assemblage de houblons aromatiques et amers. Attaque en bouche fraîche et fruitée relevée par une douce amertume.",
             "prix":"4.5€",
             "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/bieres/ocale_blonde.jpg"
                 },
-            {"nom":"Ocale IPA",
-            "description":"bla bla bla",
+            {"nom":"OC’ALE IPA",
+            "description":"Malt d’orge Pilsen. Assemblage de houblons aromatiques et amers. Houblonnage à cru. Attaque en bouche fraîche et fruitée relevée par une amertume prononcée. Des arômes persistants d’agrumes et de fruits exotiques apportés par le houblonnage à cru.",
             "prix":"4.5€",
             "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/bieres/ocale_ipa.jpg"
                 },
-            {"nom":"producteur C blanche",
-            "description":"bla bla bla",
+            {"nom":"19 30 Ambrée",
+            "description":"La 19 30 Ambrée est une bière type English Amber Ale à la robe orangée. Au nez, vous pourrez percevoir des notes de cacao et de fruits jaunes. En bouche, ses houblons floraux anglais lui garantissent une belle rondeur et une faible amertume.",
             "prix":"4.5€",
             "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/bieres/bouteille_1930_33cl_ambree.jpg"
                 },
-            {"nom":"producteur C blonde",
-            "description":"bla bla bla",
+            {"nom":"19 30 Brune",
+            "description":"la 19 30 Brune est une bière sobre et gourmande à la robe sombre. Cette Sweet Stout brassée avec des houblons aromatiques révèlent des aromes délicatement fruités et caramélisés sur des notes de café et de cacao intenses.",
             "prix":"4.5€",
             "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
-                },
-            {"nom":"producteur C IPA",
-            "description":"bla bla bla",
-            "prix":"4.5€",
-            "qtt":"33cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/bieres/bouteille_1930_33cl_brune.jpg"
                 }
 ]
 
@@ -131,19 +119,19 @@ coktail = [{"nom":"Mojito",
             "description":"bla bla bla",
             "prix":"8€",
             "qtt":"25cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/coktail/mojito.jpg"
                 },
             {"nom":"Apérol Spritz",
             "description":"bla bla bla",
             "prix":"8€",
             "qtt":"25cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/coktail/aperolspritz.jpg"
                 },
             {"nom":"Moscow Mule",
             "description":"bla bla bla",
             "prix":"8€",
             "qtt":"25cl",
-            "image":"assets/img/no_img.jpg"
+            "image":"assets/img/carte/boissons/coktail/moscowmule.jpg"
                 }
 ]
 
